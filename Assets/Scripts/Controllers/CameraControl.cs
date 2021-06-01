3ae0fce0e8bbdcc0961012458824f347a0bc0923
Артем Богунов <bogunov_aa@edu.bstu.ru>
2021-06-01 23:15:21 +0300

    code: code style

--- Assets/Scripts/Controllers/CameraControl.cs
+++ Assets/Scripts/Controllers/CameraControl.cs
@@ -4,41 +4,46 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    private Camera _camera;
-
     [SerializeField]private int _minZoom = 0;
     [SerializeField]private int _maxZoom=12000;
 
+    private Camera _camera;
     private bool _moveCamera;
+
     void Start()
     {
         _camera=GetComponent<Camera>();
     }
-
-    // HYI
 
     void Update()
     {
         Zoom();
         Move();
     }
-    public void Zoom(){
+    public void Zoom()
+    {
         float mw = Input.GetAxis("Mouse ScrollWheel");
         float newY = Mathf.Clamp(transform.position.y+mw*100,_minZoom,_maxZoom);
-        if(mw!=0){
-               transform.position=new Vector3(transform.position.x,newY,transform.position.z);
-        }
+
+        if(mw!=0)    
+            transform.position=new Vector3(transform.position.x,newY,transform.position.z);        
     }
-    public void Move(){
+
+    public void Move()
+    {
         float posX = Input.GetAxis("Mouse X")*8;
         float posY = Input.GetAxis("Mouse Y")*8;
-        if(Input.GetMouseButtonDown(0)){
+
+        if(Input.GetMouseButtonDown(0))
+        {
             _moveCamera=true;
         }
-        if(_moveCamera){
+        if(_moveCamera)
+        {
             _camera.transform.position-= new Vector3(posX,0,posY);
         }
-        if(Input.GetMouseButtonUp(0)){
+        if(Input.GetMouseButtonUp(0))
+        {
              _moveCamera=false;
         }
     }
